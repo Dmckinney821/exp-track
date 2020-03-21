310af272d1a0f638232e87e70c61c92676853631
Dan McKinney <3dhdaniel@gmail.com>
2020-03-20 21:51:00 -0400

    mo

--- script.js
+++ script.js
@@ -16,6 +16,32 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+function addTransaction(e) {
+    e.preventDefault();
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add text and amount')
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        }
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction)
+
+
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
 function addTransactionDOM(transaction) {
     const sign = transaction.amount < 0 ? '-' : '+';
 
@@ -24,29 +50,35 @@ function addTransactionDOM(transaction) {
 
     item.innerHTML = `
         ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-        <button class='delete-btn'>x</button>
+        <button class='delete-btn' onclick='removeTransaction(${transaction.id})'>x</button>
     `
     list.appendChild(item)
 }
 
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount)
-    console.log(amounts);
+    
     const total = amounts.reduce((acc, item) => (acc += item),0).toFixed(2);
     const income = amounts
         .filter(item => item > 0)
         .reduce((acc, item) => (acc += item), 0)
         .toFixed(2)
-        console.log(income)
+        
     const expense = (amounts
         .filter(item => item < 0)
         .reduce((acc, item) => (acc += item), 0)* -1)
         .toFixed(2)
-        console.log(expense)
+        
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minues.innerText = `$${expense}`;
 }
+
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    init()
+}
+
 function init() {
     list.innerHTML = '';
 
@@ -55,3 +87,5 @@ function init() {
 }
 
 init()
+
+form.addEventListener('submit', addTransaction);
